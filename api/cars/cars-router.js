f91de8a2fc6d446634d66c537e83b49055be94ff
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-13 20:51:37 -0400

    get all wired and new VIN seeds

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,35 @@
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const md = require('./cars-middleware');
 
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/:id', md.checkCarId, async (req, res, next) => {
+  res.json(req.car);
+});
+router.post('/', async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+router.put('/:id', async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+router.delete('/:id', async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
